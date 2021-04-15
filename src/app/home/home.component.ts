b69a5d12c039a5e5fc77ac9ed727d5f9d1175e69
Kevin Grady <kevingrady@kevins-mbp.lan>
2021-04-15 01:00:09 -0400

    got stuff to show up

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,14 @@
+import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import {  takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+export interface Card {
+  heroesUrl: string;
+  textfile: string;
+  date: any;
+}
 
 @Component({
   selector: 'app-home',
@@ -7,9 +17,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  cards = [];
+  stringJson: any;
+  stringObject: [];
 
-  ngOnInit(): void {
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+
+    this.dataService.sendGetRequest().subscribe((data: any[])=>{
+      console.log(data)
+
+      // Add safe, URL encoded_page parameter 
+      const options = { params: new HttpParams({fromString: "_page=1&_limit=20"}) };
+
+      this.cards = data['cards'];
+      
+      // Convert String obect to JSON
+      this.stringJson = JSON.stringify(this.cards);
+      console.log("String json object :", this.stringJson);
+      console.log("Type :", typeof this.stringJson);
+  
+      // ConvertjSON to an object
+      this.stringObject = JSON.parse(this.stringJson);
+      console.log("JSON object -", this.stringObject);
+
+    })  
+
   }
 
 }
