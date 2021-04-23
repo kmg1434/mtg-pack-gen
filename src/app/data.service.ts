57ce32192f3cd104266b0ab7dd160ec04856cf85
Kevin Grady <kmg1434@gmail.com>
2021-04-22 23:36:32 -0400

    add folder structure, pagination attempt, and add getCommonCard() to data service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 
 import {  throwError } from 'rxjs';
-import { retry, catchError } from 'rxjs/operators';
+import { retry, catchError, tap } from 'rxjs/operators';
 import { $ } from 'protractor';
-
 
 
 @Injectable({
@@ -12,29 +11,80 @@ import { $ } from 'protractor';
 })
 export class DataService {
 
+  // Pagination
+  public first: string = "";
+  public prev: string = "";
+  public next: string = "";
+  public last: string = "";
+
   private REST_API_SERVER = "https://api.magicthegathering.io/v1";
   private LEGAL_SETS = "set=ALA,RTR,GTC,KTK"; // defined by Mark, expansions will need to be added here
-  colorIdentity = "colorIdentity=B";
-  rarity = "rarity=rare";
+  public colorIdentity = "colorIdentity=B";
+  public rareRarity = "rarity=rare,mythic";
+  public uncomonRarity = "rarity=uncommon";
+  public commonRarity = "rarity=common";
 
-  private queryParamString = `?${this.LEGAL_SETS}&${this.colorIdentity}&${this.rarity}`
+  // amount of card rarities per booster pack
+  public commonCount = 10;
+  public uncommonCount = 3;
+  public rareCount = 1; // or mythic
+
+  public page = "page=1";
+  public pageSize = "pageSize=1";
+  public pageParams = `${this.page}&${this.pageSize}`;
+
+  private getAllQueryParamString = `?${this.LEGAL_SETS}&${this.colorIdentity}&${this.rareRarity}&${this.pageParams}`;
 
   constructor(private httpClient: HttpClient) { }
 
-    handleError(error: HttpErrorResponse) {
-      let errorMessage = 'Unknown error!';
-      if (error.error instanceof ErrorEvent) {
-        // Client-side errors
-        errorMessage = `Error: ${error.error.message}`;
-      } else {
-        // Server-side errors
-        errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
-      }
-      window.alert(errorMessage);
-      return throwError(errorMessage);
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side errors
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side errors
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
     }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
 
   public getAllCards(){
-    return this.httpClient.get(`${ this.REST_API_SERVER }/cards/${ this.queryParamString }`).pipe(retry(3), catchError(this.handleError));
+    // Add safe, URL encoded_page parameter 
+    const options = { params: new HttpParams({fromString: "page=1&pageSize=20"}) };
+
+    return this.httpClient.get(`${ this.REST_API_SERVER }/cards/${ this.getAllQueryParamString }`)
+                          .pipe(retry(3), catchError(this.handleError));
   }
+
+  private getCommonQueryParamString = `?${this.LEGAL_SETS}&${this.colorIdentity}&${this.commonRarity}&${this.pageParams}`
+
+  public getCommonCard(){
+    return this.httpClient.get(`${ this.REST_API_SERVER }/cards/${ this.getCommonQueryParamString }`)
+    .pipe(retry(3), catchError(this.handleError));
+  }
+
+  // pagination parse function
+  parseLinkHeader(header) {
+    if (header.length == 0) {
+      return ;
+    }
+
+    let parts = header.split(',');
+    var links = {};
+    parts.forEach( p => {
+      let section = p.split(';');
+      var url = section[0].replace(/<(.*)>/, '$1').trim();
+      var name = section[1].replace(/rel="(.*)"/, '$1').trim();
+      links[name] = url;
+
+    });
+
+    this.first  = links["first"];
+    this.last   = links["last"];
+    this.prev   = links["prev"];
+    this.next   = links["next"]; 
+  }
+
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { HttpParams } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataService } from '../data.service';
 import {  takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
@@ -15,35 +15,39 @@ export interface Card {
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   cards = [];
   stringJson: any;
   stringObject: [];
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
 
-    this.dataService.getAllCards().subscribe((data: any[])=>{
+    this.dataService.getAllCards().pipe(takeUntil(this.destroy$)).subscribe((data: any[])=>{
       console.log(data)
 
       // Add safe, URL encoded_page parameter 
-      const options = { params: new HttpParams({fromString: "_page=1&_limit=20"}) };
+      // const options = { params: new HttpParams({fromString: "_page=1&_limit=20"}) };
 
       this.cards = data['cards'];
       
       // Convert String obect to JSON
       this.stringJson = JSON.stringify(this.cards);
-      console.log("String json object :", this.stringJson);
-      // console.log("Type :", typeof this.stringJson);
-  
-      // ConvertjSON to an object
+      // ConvertjSON to an object (that's compatable with *ngFor)
       this.stringObject = JSON.parse(this.stringJson);
       console.log("JSON object -", this.stringObject);
 
     })  
 
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    // Unsubscribe from the subject
+    this.destroy$.unsubscribe();
   }
 
 }
